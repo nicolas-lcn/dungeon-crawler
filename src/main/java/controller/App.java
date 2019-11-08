0ebf535d04953afa23408cdffaa6503c045a17e2
BasileCouetoux <basile.couetoux@univ-amu.fr>
2019-11-08 08:03:43 +0100

    reorganisation

--- src/main/java/controller/App.java
+++ src/main/java/controller/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -12,9 +13,7 @@ import view.JavaFXView;
 import view.View;
 
 public class App extends Application {
-    /*public static void main(String[] args) {
-        new KeyBoardController().read();
-    }
+    /*
 
     /**
      * The main entry point for all JavaFX applications.
@@ -37,20 +36,11 @@ public class App extends Application {
         JavaFXView view = new JavaFXView();
         Text message = view.text;
         root.getChildren().add(message);
+        root.getChildren().add(new Canvas(400,200));
         Scene scene = new Scene(root);
         Player player = new Player(view);
-        EventHandler<? super KeyEvent> eventHandler = new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent event) {
-                switch (event.getCode()) {
-                    case UP:    player.goNorth(); break;
-                    case DOWN:  player.goNorth(); break;
-                    case LEFT:  player.goNorth(); break;
-                    case RIGHT: player.goNorth(); break;
-                }
-            }
-        } ;
-        scene.setOnKeyPressed(eventHandler);
+        JavaFXController javaFXController = new JavaFXController(player);
+        scene.setOnKeyPressed(javaFXController.eventHandler);
         primaryStage.setScene(scene);
         primaryStage.show();
 
--- src/main/java/controller/JavaFXController.java
+++ src/main/java/controller/JavaFXController.java
@@ -1,4 +1,23 @@
 package controller;
 
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyEvent;
+import model.Player;
+
 public class JavaFXController {
+    EventHandler<? super KeyEvent> eventHandler;
+
+    JavaFXController(Player player){
+
+     eventHandler = new EventHandler<KeyEvent>() {
+        @Override
+        public void handle(KeyEvent event) {
+            switch (event.getCode()) {
+                case UP:    player.goNorth(); break;
+                case DOWN:  player.goNorth(); break;
+                case LEFT:  player.goNorth(); break;
+                case RIGHT: player.goNorth(); break;
+            }
+        }
+    }; }
 }
